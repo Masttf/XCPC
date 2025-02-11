39ab32b0e69f2bef08300fc2b94d5bb15a8f6ac1
xiaolinaini <111647759+Masttf@users.noreply.github.com>
2025-02-11 19:51:20 +0800

    2025-02-11

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ git commit -m "$(date +'%Y-%m-%d')"
 git push origin master
 if [ $? -ne 0 ]; then
     echo 'fetch'
-    # git fetch origin
-    # git rebase origin/master
+    git fetch origin
+    git rebase origin/master
     echo 'push'
-    # git push origin master
+    git push origin master
 fi
